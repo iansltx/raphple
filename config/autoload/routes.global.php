3e15c0b5f01d400d64bf4bed6b90a1b18f2344b8
Ian Littman <ian@cloudyhills.com>
2016-10-13 17:15:12 -0500

    Switch to pogrammatic declaration of routes and middlewares

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -12,45 +12,4 @@ return [
             App\Action\CompleteAction::class => App\Action\CompleteAction::class,
         ]
     ],
-
-    'routes' => [
-        // webhooks
-        [
-            'name' => 'Twilio',
-            'path' => '/twilio',
-            'middleware' => App\Action\TwilioAction::class,
-            'allowed_methods' => ['POST'],
-        ],
-        [
-            'name' => 'Nexmo',
-            'path' => '/nexmo',
-            'middleware' => App\Action\NexmoAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        // main site
-        [
-            'name' => 'Home',
-            'path' => '/',
-            'middleware' => App\Action\HomeAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'Create',
-            'path' => '/',
-            'middleware' => App\Action\CreateAction::class,
-            'allowed_methods' => ['POST'],
-        ],
-        [
-            'name' => 'Get',
-            'path' => '/:id',
-            'middleware' => App\Action\GetAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'Complete',
-            'path' => '/:id',
-            'middleware' => App\Action\CompleteAction::class,
-            'allowed_methods' => ['POST'],
-        ],
-    ],
 ];
--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace App;
+
+use Zend\Expressive\Helper;
+use App\Action;
+use Zend\Expressive\Application;
+
 // Delegate static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server'
     && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
@@ -14,5 +20,20 @@ require __DIR__ . '/../vendor/autoload.php';
 $container = require __DIR__ . '/../config/container.php';
 
 /** @var \Zend\Expressive\Application $app */
-$app = $container->get(\Zend\Expressive\Application::class);
+$app = $container->get(Application::class);
+
+$app->pipe(Helper\ServerUrlMiddleware::class);
+$app->pipeRoutingMiddleware();
+$app->pipe(Helper\UrlHelperMiddleware::class);
+$app->pipeDispatchMiddleware();
+
+// webhooks
+$app->post('/twilio', Action\TwilioAction::class, 'Twilio');
+$app->get('/nexmo', Action\NexmoAction::class, 'Nexmo');
+// normal site
+$app->get('/', Action\HomeAction::class, 'Home');
+$app->post('/', Action\CreateAction::class, 'Create');
+$app->get('/{id}', Action\GetAction::class, 'Get');
+$app->post('/{id}', Action\CompleteAction::class, 'Complete');
+
 $app->run();
